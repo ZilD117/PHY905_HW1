fdbb6fca2acdfd82f0f8a22dcd9363a9c06174de
ZilD117 <dongzil1@msu.edu>
2023-03-05 20:17:55 -0500

    changed a minus sign to plus sign in line 33

--- crashing_program.py
+++ crashing_program.py
@@ -30,7 +30,7 @@ def deriv(f,x,h,points):
     elif points==3:
         return (f(x+h)-f(x-h))/(2.0*h)
     elif points==5:
-        return (f(x-2.0*h)-8.0*f(x-h)+8.0*f(x+h)-f(x+2.0*h))/(12.0*h)
+        return (f(x-2.0*h)+8.0*f(x-h)+8.0*f(x+h)-f(x+2.0*h))/(12.0*h)
     else:
         print("ERROR: deriv() cannot handle this many points:", points, flush=True)
         exit()
