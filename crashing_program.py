45e5ded50167f3036aa9076cdc8f6ab1cc29ac68
Sierra Casten <sierracasten@Sierras-MacBook-Air-3.local>
2023-03-05 20:17:40 -0500

    added set of parenthesis to line 33

--- crashing_program.py
+++ crashing_program.py
@@ -30,7 +30,7 @@ def deriv(f,x,h,points):
     elif points==3:
         return (f(x+h)-f(x-h))/(2.0*h)
     elif points==5:
-        return (f(x-2.0*h)-8.0*f(x-h)+8.0*f(x+h)-f(x+2.0*h))/(12.0*h)
+        return ((f(x-2.0*h)-8.0*f(x-h)+8.0*f(x+h)-f(x+2.0*h))/(12.0*h))
     else:
         print("ERROR: deriv() cannot handle this many points:", points, flush=True)
         exit()
@@ -133,7 +133,6 @@ def trapezoid(f,start,end,epsilon=1.0e-6,itmax=100,debug=False):
     else:
         return new_integrand, this_iter
 
-
 def main():
 
     print("\nStarting our calculation.  Yay!\n")
@@ -151,12 +150,6 @@ def main():
     print("\nIntegral from 0 to ", root, "is", integrand, ", which took",
               integral_iters, "iterations.\n")
 
-def test1():
-    root, root_iters = secant(fofx,guess = -2,points=5,debug=False,itmax=20)
-    result = root
-    assert result >=100
-
 # execute main() if this is being run as a script!
 if __name__ == "__main__":
     main()
-
